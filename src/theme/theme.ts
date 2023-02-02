0ac01f159d112bde6d076ea91ecf40c3b3f1248f
Wesley Silva <wesley.allansilva@gmail.com>
2023-02-02 01:22:11 -0300

    feat(theme): add theme provider
    Add theme to styled-components for the project

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,11 @@
 export const theme = {
-  colorBlue200: '#0B4F6C',
-  colorGray200: '#DCE0D9',
-  colorGray100: 'rgba(176, 178, 184, 0.79)',
+  colorPurple500: '#3F1D38',
+  colorPurple200: '#DFCCDA',
+  colorBlue200: '#8ab7da',
+  colorGray500: '#8E7E98',
+  colorGray200: '#DCDFF8',
+  colorGray100: '#DCE0D9',
   colorWhite: '#FBFBFF',
-  colorYellow500: '#FFE548',
+  colorYellow500: '#FCAD48',
+  colorRed500: '#FF4F66',
 };
