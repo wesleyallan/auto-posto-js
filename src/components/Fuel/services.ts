c66a003d3c44e6d72c9487be22adfcdd80c8a4a0
Wesley Silva <wesley.allansilva@gmail.com>
2023-02-03 01:43:18 -0300

    feat(component): add put method
    Partial or complete price update method added

--- src/components/Fuel/services.ts
+++ src/components/Fuel/services.ts
@@ -1,7 +1,14 @@
 import { Api } from '../../services/api';
+import type { IFuel } from './types';
 
 export const getFuel = async () => {
   const request = await Api.get('fuels');
+
+  return request.data;
+};
+
+export const updateFuel = async (fuel: IFuel) => {
+  const request = await Api.put(`fuels/${fuel.id}`, fuel);
 
   return request.data;
 };
