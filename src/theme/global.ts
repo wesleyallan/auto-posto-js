947a3c734c5da796f6ab810bad21a22b4260b69b
Wesley Silva <wesley.allansilva@gmail.com>
2023-02-17 01:26:12 -0300

    feat(global styles): add global styles with styled-components
    removing css files and adding global css with styled-components

---
+++ src/theme/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    font-family: 'Rubik', sans-serif;
+    font-synthesis: none;
+    font-size: 100%;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+  }
+`;
